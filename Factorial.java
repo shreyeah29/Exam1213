8165cc3719e34de58b36eab714a738cc95bc845c
shreyeah29 <shreyareddys29@gmail.com>
2025-12-03 10:54:27 +0530

    Updatwee Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,7 +1,7 @@
 public class Factorial{
 public static long compute(int n){
 
-    if(n < 0) throw new IllegalArgumentException("Negative number not allowedd");
+    if(n < 0) throw new IllegalArgumentException("Negative number not allowesfedd");
 
 long result=1;
 for(int i=2;i<=n;i++){
